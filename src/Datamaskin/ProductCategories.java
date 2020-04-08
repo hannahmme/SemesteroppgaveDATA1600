78590a603f6e706cfcc52fa8e7e6787c24c484bf
Amalie Leiknes <s340559@oslomet.no>
2020-04-08 11:01:19 +0200

    endret fxmns til versjon 8.0.171

--- src/Datamaskin/ProductCategories.java
+++ src/Datamaskin/ProductCategories.java
@@ -19,28 +19,28 @@ public class ProductCategories {
     }
 
     // alle de forskjellige kategoriene et produkt kan være innenfor
-    public static ArrayList<Product> Skjermkort = new ArrayList<>();
+    public static ArrayList<Product> GraphicCard = new ArrayList<>();
     public static ArrayList<Product> Memorycard = new ArrayList<>();
-    public static ArrayList<Product> Tastatur = new ArrayList<>();
+    public static ArrayList<Product> Keyboard = new ArrayList<>();
     public static ArrayList<Product> Mouse = new ArrayList<>();
     public static ArrayList<Product> OtherComponents = new ArrayList<>();
 
 
     public static void setData(Product etProduct, String category){
-        if(category == "Skjermkort") {
-            Skjermkort.add(etProduct);
+        if(category == "GraphicCard") {
+            GraphicCard.add(etProduct);
         } else if (category == "Memorycard"){
             Memorycard.add(etProduct);
-        } else if (category == "Tastatur"){
-            Tastatur.add(etProduct);
+        } else if (category == "Keyboard"){
+            Keyboard.add(etProduct);
         } else if (category == "Mouse"){
             Mouse.add(etProduct);
         } else if (category == "Other Components"){
             OtherComponents.add(etProduct);
         }
 
-        System.out.println(Skjermkort);
-        System.out.println(Memorycard);
+        System.out.println(GraphicCard.toString());
+        System.out.println(Memorycard.toString());
 
     }
 

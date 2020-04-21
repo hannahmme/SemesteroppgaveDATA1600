834bbb2056192407e5a01fa66f2282c25414f68e
Hannah M.M. Eriksen <hannah_eriksen@hotmail.com>
2020-04-21 11:21:02 +0200

    Gjør bildene i buttons trykkbare. Oppretter klasse ImageClass for å redusere Controller

--- src/Datamaskin/FXML/MainpageController.java
+++ src/Datamaskin/FXML/MainpageController.java
@@ -4,8 +4,7 @@ import Datamaskin.CustomerValidator;
 import Datamaskin.Exceptions.InvalidEmailException;
 import Datamaskin.Page;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.event.ActionEvent;
+import Datamaskin.images.ImageClass;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -18,7 +17,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -26,27 +27,34 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainpageController implements Initializable {
-    @FXML private Button toEnduserPage;
-
+    @FXML private Button btnEnduserPage;
     @FXML private TextField txtUsername;
     @FXML private TextField txtPassword;
-    @FXML private Button toSuperuserPage;
-
+    @FXML private Button btnSuperuserPage;
     @FXML private TextField txtUserEmail;
     @FXML private TextField txtUserPassword;
     @FXML private Button btnUserOrders;
-
     @FXML private Label lblErrorEmail;
 
 
+    private ImageClass image = new ImageClass();
+
+    //Dette kastes pga bildene som lastes inn når siden lastes inn. Skal sjekke om det kan fjernes evt  - Hannah
     public MainpageController() throws FileNotFoundException {
     }
 
-    @FXML void btnUserOrders(ActionEvent event) throws IOException {
-        try{
-            String email = txtUserEmail.getText();
+    //Todo: SKal denne brukes noe sted? - Hannah
+    // bruke denne med try/catch for å legge til en verdi som skal brukes for å filtrere userSpecific order
+    public void checkEmail() throws InvalidEmailException {
+        String email = txtUserEmail.getText();
+        CustomerValidator.validateEmail(email);
+    }
 
-            if(!CustomerValidator.validateEmail(email)){
+    @FXML
+    void toUserOrders() throws IOException {
+        try {
+            String email = txtUserEmail.getText();
+            if (!CustomerValidator.validateEmail(email)) {
                 throw new InvalidEmailException("Skriv inn gyldig e-postadresse");
             } else {
                 Stage primaryStage = (Stage) btnUserOrders.getScene().getWindow();
@@ -54,100 +62,82 @@ public class MainpageController implements Initializable {
                 Page.toUserspesificOrder(primaryStage, root);
                 primaryStage.show();
             }
-        }
-        catch(InvalidEmailException e){
+        } catch (InvalidEmailException e) {
             lblErrorEmail.setText(e.getMessage());
-        }
-    }
-
-    // bruke denne med try/catch for å legge til en verdi som skal brukes for å filtrere userSpecific order
-    public void checkEmail() throws InvalidEmailException {
-        String email = txtUserEmail.getText();
-        CustomerValidator.validateEmail(email);
-    }
-
-
-    //Metode som gjør at du kan gå til neste side ved å trykke på "Enter-knappen" - Hannah
-    @FXML
-    void btnLoginEnter(KeyEvent event) throws IOException {
-        if (event.getCode().equals(KeyCode.ENTER)) {
-            Stage primaryStage = (Stage) toSuperuserPage.getScene().getWindow();
-            Parent root = FXMLLoader.load(getClass().getResource("SuperuserPage.fxml"));
-            Page.toSuperuserpage(primaryStage, root);
-        }
-    }
-
-    @FXML
-    void btnUserOrdersEnter(KeyEvent event) throws IOException {
-        if (event.getCode().equals(KeyCode.ENTER)) {
-            Stage primaryStage = (Stage) btnUserOrders.getScene().getWindow();
-            Parent root = FXMLLoader.load(getClass().getResource("UserspesificOrder.fxml"));
-            Page.toUserspesificOrder(primaryStage, root);
-        }
-    }
-
-    @FXML
-    void btnBuildEnter(KeyEvent event) throws IOException {
-        if (event.getCode().equals(KeyCode.ENTER)) {
-            Stage primaryStage = (Stage) toEnduserPage.getScene().getWindow();
-            Parent root = FXMLLoader.load(getClass().getResource("EnduserPage.fxml"));
-            Page.toEnduserPage(primaryStage, root);
-        }
+            }
     }
 
     // metode som åpner ny scene til superbrukersiden
-    @FXML void toEnduserPage(ActionEvent event) throws IOException {
-        Stage primaryStage = (Stage) toEnduserPage.getScene().getWindow();
+    @FXML
+    void toEnduserPage() throws IOException {
+        Stage primaryStage = (Stage) btnEnduserPage.getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getResource("EnduserPage.fxml"));
         Page.toEnduserPage(primaryStage, root);
+        primaryStage.show();
     }
 
-    // metode som åpner ny scene til sluttbrukersiden
-    @FXML void toSuperUserPage(ActionEvent event) throws IOException {
-        String username = null;
-        String password = null;
+    // metode som åpner ny scene til sluttbrukersiden (endret versjon av metoden) - hannah
+    @FXML
+    void toSuperUserPage() throws IOException {
         try {
-            username = txtUsername.getText();
-            password = txtPassword.getText();
-        } catch(Exception e){
-            e.printStackTrace();
-        }
-
-        if (username.matches("admin") && password.matches("admin")) {
-            try {
-                Stage primaryStage = (Stage) toSuperuserPage.getScene().getWindow();
-                Parent root = FXMLLoader.load(getClass().getResource("SuperuserPage.fxml"));
-                Page.toSuperuserpage(primaryStage, root);
-            } catch (Exception e) {
-                e.printStackTrace();
+           String username = txtUsername.getText();
+           String password = txtPassword.getText();
+            if(username == null || password == null){
+                throw new NullPointerException("Noe gikk galt ved innlasting av brukernavn/passord");
+            } else{
+                boolean isMatching = username.matches("admin") && password.matches("admin");
+                if(isMatching){
+                    Stage primaryStage = (Stage) btnSuperuserPage.getScene().getWindow();
+                    Parent root = FXMLLoader.load(getClass().getResource("SuperuserPage.fxml"));
+                    Page.toSuperuserpage(primaryStage, root);
+                    primaryStage.show();
+                }
             }
+        }catch(NullPointerException npe){
+            System.err.println(npe.getMessage());
         }
     }
 
+//Kode gjør at bildet kan trykkes på og gå videre til neste side
+    @FXML
+    void imgSuperUserPage(MouseEvent event) throws IOException {
+        toSuperUserPage();
+    }
+    @FXML
+    void imgEnduserPage(MouseEvent event) throws IOException {
+        toEnduserPage();
+    }
+    @FXML
+    void btnUserOrdersEnter(KeyEvent event) throws IOException {
+        if (event.getCode().equals(KeyCode.ENTER)) {
+            toUserOrders();
+        }
+    }
+    @FXML
+    void btnEnduserPageEnter(KeyEvent event) throws IOException {
+        if (event.getCode().equals(KeyCode.ENTER)) {
+            toEnduserPage();
+        }
+    }
+    @FXML
+    void btnSuperUserPage(KeyEvent event) throws IOException {
+        if (event.getCode().equals(KeyCode.ENTER)) {
+            toSuperUserPage();
+        }
+    }
 
     // kode for bildene som ligger på hovedsiden
     @FXML private ImageView hardwareImageView;
-    private Image hardwareImage = createImage("./src/Datamaskin/images/hardware.jpg");
+    private Image hardwareImage = image.createImage("./src/Datamaskin/images/hardware.jpg");
 
     @FXML private ImageView orderImageView;
-    private Image orderImage = createImage("./src/Datamaskin/images/order.png");
+    private Image orderImage = image.createImage("./src/Datamaskin/images/order.png");
 
     @FXML private ImageView buildImageView;
-    private Image hammerImage = createImage("./src/Datamaskin/images/hammer.png");
+    private Image hammerImage = image.createImage("./src/Datamaskin/images/hammer.png");
 
     @FXML private ImageView adminImageView;
-    private Image adminImage = createImage("./src/Datamaskin/images/admin.png");
-
-    //metode som oppretter et bilde via path og returnerer et bilde
-    private Image createImage(String path) throws FileNotFoundException {
-        FileInputStream imageStream = new FileInputStream(path);
-        return new Image(imageStream);
-    }
-
-    //metode som kobler imageviewet med bildet - hannah
-    private void setImageView(ImageView iv, Image image){
-        iv.setImage(image);
-    }
+    private Image adminImage = image.createImage("./src/Datamaskin/images/admin.png");
 
     //Effekt for å blurre bilde på mainpage (det blå bildet)
     private DropShadow shadowEffect = new DropShadow();
@@ -155,10 +145,10 @@ public class MainpageController implements Initializable {
     //Bilder settes i ImageViewet når siden lastes inn, samt effekt på bildet
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        setImageView(orderImageView, orderImage);
-        setImageView(buildImageView, hammerImage);
-        setImageView(adminImageView, adminImage);
-        setImageView(hardwareImageView, hardwareImage);
+        image.setImageView(orderImageView, orderImage);
+        image.setImageView(buildImageView, hammerImage);
+        image.setImageView(adminImageView, adminImage);
+        image.setImageView(hardwareImageView, hardwareImage);
         shadowEffect.setRadius(50);
         shadowEffect.setWidth(50);
         shadowEffect.setHeight(25);

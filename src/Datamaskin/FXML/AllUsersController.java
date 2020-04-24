3df1f9b2c2458f5e170d4a0b389b05358859b256
Amalie Leiknes <s340559@oslomet.no>
2020-04-24 08:01:35 +0200

    rthnfg

--- src/Datamaskin/FXML/AllUsersController.java
+++ src/Datamaskin/FXML/AllUsersController.java
@@ -1,26 +1,39 @@
 package Datamaskin.FXML;
 
+import Datamaskin.Page;
 import Datamaskin.customer.Customer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AllUsersController {
 
     @FXML private TableView<Customer> customerTV;
-
     @FXML private TableColumn<String, Customer> emailColumn;
-
     @FXML private TableColumn<String, Customer> passwordColumn;
+    @FXML private Button toSuperuserpage;
 
-    @FXML private Button btnGoBack;
 
+    //knappen "tilbake" tar brukeren med tilbake til menysiden for superbruker
+    @FXML void toSuperuserpage() throws IOException {
+        Stage primaryStage = (Stage) toSuperuserpage.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("SuperuserPage.fxml"));
+        Page.toSuperuserpage(primaryStage, root);
+    }
 
-    // knapp som tar bruker til forrige side
-    @FXML void GoBack(ActionEvent event) {
-
+    @FXML void btnSuperUserPageEnter(KeyEvent event) throws IOException {
+        if (event.getCode().equals(KeyCode.ENTER)) {
+            toSuperuserpage();
+        }
     }
 
 }

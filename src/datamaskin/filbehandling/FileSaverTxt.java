647ff10f3a45ad71ddb8709a6ab293432d116177
Hannah M.M. Eriksen <hannah_eriksen@hotmail.com>
2020-04-27 17:00:06 +0200

    Dette er metoder som appender til AllOrders.csv. Forrige commit leser fra fil

--- src/datamaskin/filbehandling/FileSaverTxt.java
+++ src/datamaskin/filbehandling/FileSaverTxt.java
@@ -3,6 +3,7 @@ package datamaskin.filbehandling;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 
 //fileSaverTxt implementerer metoden fra interfaces iFileSaver, og fyller method-body her.
 public class FileSaverTxt implements iFileSaver{
@@ -11,4 +12,13 @@ public class FileSaverTxt implements iFileSaver{
         Files.write(path, orderToSave.getBytes());
     }
 
+//StandardOpenOption.APPEND gjør at det som lagres, legges automatisk til på siste linje
+    @Override
+    public void appendToFile(String str, Path path) throws IOException {
+        Files.write(path, str.getBytes(), StandardOpenOption.APPEND);
+    }
+
 }
+
+
+
--- src/datamaskin/filbehandling/iFileSaver.java
+++ src/datamaskin/filbehandling/iFileSaver.java
@@ -3,8 +3,13 @@ package datamaskin.filbehandling;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 //hensikten med interfacet er at denne metoden kan implementeres der den trengs
 public interface iFileSaver {
     public void saveToFile(String string, Path path) throws IOException;
+
+    public void appendToFile(String str, Path path) throws IOException;
+
 }
+

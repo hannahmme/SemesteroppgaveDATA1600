c84838c89f0cb3fbe58db39262b1cb6efb11136d
Amalie Leiknes <s340559@oslomet.no>
2020-05-04 12:57:36 +0200

    diverse design

--- src/datamaskin/filbehandling/binarysaving/ProductFormatter.java
+++ src/datamaskin/filbehandling/binarysaving/ProductFormatter.java
@@ -1,4 +1,4 @@
-package datamaskin.filbehandling;
+/*package datamaskin.filbehandling.binarysaving;
 
 import datamaskin.product.Product;
 
@@ -25,3 +25,4 @@ public class ProductFormatter {
         return stringOfProducts.toString();
     }
 }
+*/
